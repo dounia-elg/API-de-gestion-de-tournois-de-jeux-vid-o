554d2b0764267364ac026b25b0b61357218f565a
dounia-elg <dounia.elg20@gmail.com>
2025-03-24 10:14:43 +0000

    Create the tournament request validator

---
+++ tournois-api/app/Http/Requests/Tournament/CreateTournamentRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Tournament;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTournamentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_date' => 'required|date|after_or_equal:today',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ];
+    }
+}
