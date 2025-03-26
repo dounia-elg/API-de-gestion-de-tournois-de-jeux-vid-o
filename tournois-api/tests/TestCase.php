65a2f0c18c3dbba8f81a2515a6fc61d51c1d7115
dounia-elg <dounia.elg20@gmail.com>
2025-03-26 07:15:36 +0000

    create the testing database

--- tournois-api/tests/TestCase.php
+++ tournois-api/tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Ensure we're using the test database
+        $this->app['config']->set('database.default', 'pgsql_testing');
+    }
 }
